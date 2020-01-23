46e3492d564502e9007753e18c0bcf52ea968317
Steve Adams <irwtdvoys@gmail.com>
2020-01-23 12:43:30 +0000

    Add validate method to abstract constraint that throws exceptions

--- src/Validation/Constraint.php
+++ src/Validation/Constraint.php
@@ -2,11 +2,20 @@
 	namespace Bolt\Validation;
 
 	use Bolt\Base;
+	use Bolt\Exceptions\Validation as Exception;
 
 	abstract class Constraint extends Base
 	{
 		public $message;
 
 		abstract public function isValid($value): bool;
+
+		public function validate($value): void
+		{
+			if ($this->isValid($value))
+			{
+				throw new Exception($this->message());
+			}
+		}
 	}
 ?>
