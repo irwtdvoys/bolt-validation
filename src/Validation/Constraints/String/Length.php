cad3748c709e8c2ec2e7baa043613df69aeb13be
Steve Adams <irwtdvoys@gmail.com>
2020-01-23 12:41:36 +0000

    Add some initial basic constraints

---
+++ src/Validation/Constraints/String/Length.php
@@ -0,0 +1,49 @@
+<?php
+	namespace Bolt\Api\Request\Constraints\String;
+
+	use Bolt\Api\Request\Constraint;
+
+	class Length extends Constraint
+	{
+		public $min;
+		public $max;
+
+		public function __construct($data = null)
+		{
+			parent::__construct($data);
+
+			if (!isset($this->message))
+			{
+				switch (true)
+				{
+					case isset($this->min) && !isset($this->max):
+						$this->message("Must be longer than " . $this->min() . " characters");
+						break;
+					case !isset($this->min) && isset($this->max):
+						$this->message("Must be shorter than " . $this->max() . " characters");
+						break;
+					case isset($this->min) && isset($this->max):
+						$this->message("Must be between " . $this->min() . " and " . $this->max() . " characters");
+						break;
+				}
+			}
+		}
+
+		public function isValid($value): bool
+		{
+			$result = true;
+
+			if (isset($this->min) && strlen($value) < $this->min())
+			{
+				$result = false;
+			}
+
+			if (isset($this->max) && strlen($value) > $this->max())
+			{
+				$result = false;
+			}
+
+			return $result;
+		}
+	}
+?>
